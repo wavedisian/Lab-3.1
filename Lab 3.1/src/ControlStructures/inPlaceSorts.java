27ade2125c6fcfe1db543902af880d045f11f174
BT_1N3_16 <BT_1N3_16@BT_1N3_16.bktech.local>
2018-01-17 09:29:23 -0500

    adas

--- Lab 3.1/src/ControlStructures/inPlaceSorts.java
+++ Lab 3.1/src/ControlStructures/inPlaceSorts.java
@@ -1,5 +1,46 @@
 package ControlStructures;
 
-public class inPlaceSorts {
+public class inPlaceSorts 
+{
 
+	public static void main(String[] args)
+	{
+	
+	}
+
+
+	
+	public static void insertionSort(int[] x)
+	{
+		
+	}
+	
+	public static void selectionSort(double[] x)
+	{
+		
+	}
+	
+	public static void bubbleSort(String[] x)
+	{
+		
+	}
+	
+	public static void swap(int[] a, int x, int y)
+	{
+		int s = a[x];
+		a[x] = a[y];
+		a[y] = s;
+	}
+	public static void swap(double[] a, int x, int y)
+	{
+		double s = a[x];
+		a[x] = a[y];
+		a[y] = s;
+	}
+	public static void swap(String[] a, int x, int y)
+	{
+		String s = a[x];
+		a[x] = a[y];
+		a[y] = s;
+	}
 }
