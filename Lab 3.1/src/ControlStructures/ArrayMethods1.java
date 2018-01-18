1d36dc8e7e38c37293df85fdb9f5e930d34afa32
BT_1N3_03 <BT_1N3_03@BT_1N3_03.bktech.local>
2018-01-18 09:24:03 -0500

    bubble done

--- Lab 3.1/src/ControlStructures/ArrayMethods1.java
+++ Lab 3.1/src/ControlStructures/ArrayMethods1.java
@@ -1,11 +1,20 @@
 package ControlStructures;
 
-public class inPlaceSorts 
-{
-
+public class ArrayMethods1 
+{	
 	public static void main(String[] args)
 	{
-	
+		String[] a = {"g", "b", "e", "c", "a", "f", "d", "i", "h"};
+		for(String x : a)
+		{
+			System.out.print(x);
+		}
+		System.out.println();
+		bubbleSort(a);
+		for(String x : a)
+		{
+			System.out.print(x);
+		}
 	}
 
 
@@ -22,8 +31,30 @@ public class inPlaceSorts
 	
 	public static void bubbleSort(String[] x)
 	{
-		
+		int L = x.length-1;
+		int swaps = 1;
+		while(swaps>0)
+		{
+			swaps = 1;
+			for(int i = 0; i<L; i++)
+			{
+				if(x[i].compareTo(x[i+1])>0)
+				{
+					if(i==L-1) 
+					{
+						L--;
+					}
+					swap(x, i, i+1);
+					swaps++;
+				}
+
+			}
+			swaps = swaps-1;
+			
+		}
 	}
+	
+	
 	
 	public static void swap(int[] a, int x, int y)
 	{
